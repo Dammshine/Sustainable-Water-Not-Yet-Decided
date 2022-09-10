78c0d5a69af60a578e22086b706f9d8d1daa00e8
Jiaqi <blacker0lily311@gmail.com>
2022-09-11 00:37:54 +1000

    second prototype done

--- test_frontend/map.js
+++ test_frontend/map.js
@@ -1,13 +1,14 @@
 import createGlobe from 'cobe'
 
-const globe = createGlobe(canvas, options)
+// const globe = createGlobe(canvas, options);
 
 
 
 // Drop down button functions
+const donatebtn = document.getElementById("donatebtn");
 
 // Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function(event) {
+donatebtn.addEventListener("onclick", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
     // Cancel the default action, if needed
